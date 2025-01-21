decad3553f13902e46c9f55a8a242d21cafc2bfd
ms346719 <ms346719@ohio.edu>
2025-01-21 11:59:19 -0500

    Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/**
+File: git-lab-program.cc
+Author: Mercedes SantaMaria
+Date: 01/21/25
+Description: Lab 2 code */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}
